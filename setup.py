3b31e53d67752e8c838abe72d57554c79d50904f
Vivek Gupta <vivg269@gmail.com>
2022-02-28 14:45:36 +1100

    Some changes

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name = 'Furby_p3',
     version = '1.0',
     packages = ['Furby_p3',
                 'Furby_p3.tests'],
-    scripts = ['bin/gen_furby.py', 'bin/check_furby.py', 'bin/sim_furby.py'],
+    scripts = ['bin/check_furby.py', 'bin/run_furby_generator.py'],
     install_requires = ['numpy', 'matplotlib', 'scipy', 'pyyaml'],
     )
